0b809cdf9b62b77483664ad8d10c34a6bab22325
Arlo Reichert <65920218+ArloReichert@users.noreply.github.com>
2020-06-10 12:32:28 -0500

    Update temphum.py
    made a heading for THstats.csv

--- temphum.py
+++ temphum.py
@@ -3,14 +3,24 @@ import sys
 import Adafruit_DHT
 import datetime
 import csv
+import os
 
 # call the time and date from datetime
 now = datetime.datetime.now()
+
+# file to work with
+csvFile = 'THstats.csv'
+
+# create csv heading if not already existant
+firstRow = "Temperature, Humidity, Date, Time"
+if not os.access(csvFile,os.F_OK):
+  with open(csvFile, 'a') as fd:
+    fd.write(firstRow)
 
 # get T & H values
 humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
 # append the data to existing csv or create new one if csv does not exist
-currentRow = "Temperature: {T}, Humidity: {H}, Date: {D}, Time: {t}\n".format(T=temperature,H=humidity,D=now.strftime("%Y-%m-%d"),t=now.strftime("%H:%M:%S"))
-with open('THstats.csv', 'a') as fd:
+currentRow = "{T},{H},{D},{t}\n".format(T=temperature,H=humidity,D=now.strftime("%Y-%m-%d"),t=now.strftime("%H:%M:%S"))
+with open(csvFile, 'a') as fd:
   fd.write(currentRow)
