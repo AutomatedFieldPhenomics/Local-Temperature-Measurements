7f92f578d882bdf88531319aee86a25ca4a9ec4b
ArloReichert <reichertarlo@gmail.com>
2020-06-30 19:14:30 -0500

    added supporting libraries

--- temphum11.py
+++ temphum11.py
@@ -14,13 +14,13 @@ now = datetime.datetime.now()
 csvFile = 'THstats.csv'
 
 # create csv heading if not already existant
-firstRow = "Temperature [C], Humidity [%], Date [ymd], Time"
+firstRow = "Temperature [C], Humidity [%], Date [ymd], Time\n"
 if not os.access(csvFile,os.F_OK):
   with open(csvFile, 'a') as fd:
     fd.write(firstRow)
 
 # get T & H values
-humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+humidity, temperature = Adafruit_DHT.read_retry(11, 22)
 
 # append the data to existing csv or create new one if csv does not exist
 currentRow = "{T},{H},{D},{t}\n".format(T=temperature,H=humidity,D=now.strftime("%Y-%m-%d"),t=now.strftime("%H:%M:%S"))
